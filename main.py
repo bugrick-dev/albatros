15a2936400669f40b4250fc3a5f1a512880e50f4
bugrick <72883635+bugrick-dev@users.noreply.github.com>
2026-02-24 17:11:00 +0300

    added trackbars and working on cleaner mask

--- main.py
+++ main.py
@@ -1,35 +1,71 @@
 import cv2
 import numpy as np
 
+def empty (a):
+    pass
+
 
 cap = cv2.VideoCapture(0)
 
 
+cv2.namedWindow("Settings")
+cv2.resizeWindow("Settings", 300, 250)
+
+cv2.createTrackbar("H Min", "Settings", 100, 179, empty)
+cv2.createTrackbar("H Max", "Settings", 140, 179, empty)
+cv2.createTrackbar("S Min", "Settings", 120, 255, empty)
+cv2.createTrackbar("S Max", "Settings", 255, 255, empty)
+cv2.createTrackbar("V Min", "Settings", 255, 255, empty)
+cv2.createTrackbar("V Max", "Settings", 255, 255, empty)
+    
+
+
+# clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+
 while True:
     ret, frame = cap.read()
-    
+    frame = cv2.flip(frame, 1)
+    frame = cv2.resize(frame, (320, 240))
+
+
     if not ret:
         break
 
+    
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # blur = cv2.GaussianBlur(gray, (11,11), 1)
 
     # canny = cv2.Canny(blur, 100, 150)
-
+    
+    h_min = cv2.getTrackbarPos("H Min", "Settings")
+    h_max = cv2.getTrackbarPos("H Max", "Settings")
+    s_min = cv2.getTrackbarPos("S Min", "Settings")
+    s_max = cv2.getTrackbarPos("S Max", "Settings")
+    v_min = cv2.getTrackbarPos("V Min", "Settings")
+    v_max = cv2.getTrackbarPos("V Max", "Settings")    
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_blue = np.array([100, 120, 70]) 
-    upper_blue = np.array([140, 255, 255])
+    #h, s, v = cv2.split(hsv)
+
+
+    
+
+    #v_balanced = clahe.apply(v)
+
+    #hsv_new = cv2.merge([h, s, v_balanced])
+
+    lower_blue = np.array([h_min, s_min, v_min]) 
+    upper_blue = np.array([h_max, s_max, v_max])
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
 
-    lower_red1 = np.array([0, 120, 70])
-    upper_red1 = np.array([10, 255, 255])
+    lower_red1 = np.array([0, s_min, v_min])
+    upper_red1 = np.array([10, s_max, v_max])
     mask_red1 = cv2.inRange(hsv, lower_red1, upper_red1)
 
-    lower_red2 = np.array([170, 120, 70])
-    upper_red2 = np.array([180, 255, 255])
+    lower_red2 = np.array([170, s_min, v_min])
+    upper_red2 = np.array([180, s_max, v_max])
     mask_red2 = cv2.inRange(hsv, lower_red2, upper_red2)
 
     mask_red_total = cv2.bitwise_or(mask_red1, mask_red2)
@@ -39,11 +75,13 @@ while True:
 
     # cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)     
     
+
+
     for cnt in contours:
 
         area = cv2.contourArea(cnt)
 
-        if area > 1000:
+        if area > 500:
 
             perimeter = cv2.arcLength(cnt, True)
 
@@ -67,8 +105,10 @@ while True:
                     isim = "kare"
                 else:
                     isim = "dikdortgen"
+            elif corners == 6:
+                isim = "hexagon"
             else:
-                isim = "daire"
+                isim = "circle"
             
             cv2.putText(frame, isim, (x, y - 5), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
