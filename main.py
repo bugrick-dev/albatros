09917fd6e8c15922882e44ff26e43dedc5c4ba3e
bugrick <72883635+bugrick-dev@users.noreply.github.com>
2026-02-25 17:52:10 +0300

    troubleshooting

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def empty (a):
     pass
 
 
-cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+cap = cv2.VideoCapture("/dev/video0", cv2.CAP_V4L2)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
